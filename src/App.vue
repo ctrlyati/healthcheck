a0cbea01b84d308deaab7f5fbdaa1ebb48e27e6d
ctrlyati <ctrlyati@gmail.com>
2020-10-27 09:37:51 +0700

    setup project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+    <Topbar></Topbar>
+
+    
+
+  </div>
+</template>
+
+<script>
+
+import Topbar from './components/Topbar'
+
+export default {
+  name: 'App',
+  components: {
+    Topbar
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./styles/main.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
